486eb58c0e62d9d0b5e903795bc9c86aa8035b77
Jia Yi <lee.jiayi@balliol.ox.ac.uk>
2024-10-24 16:02:10 +0100

    control merge conflict

--- control.py
+++ control.py
@@ -43,10 +43,3 @@ class Controller:
         """
         self.prev_error = 0.0
 
-
-# Example Usage
-gains = {'Kp': 0.15, 'Ki': 0.0, 'Kd': 0.6}
-controller = Controller(gains)
-
-# is there a way to get the current error 
-control_action = controller.PD_control(error)
