95a660312aaaac1dade75b35035d0e2a548ed899
Jia Yi <lee.jiayi@balliol.ox.ac.uk>
2024-10-24 15:58:24 +0100

    delete init file

--- __init__.py
+++
@@ -1,7 +0,0 @@
-# User input
-# Example Usage
-gains = {'Kp': 0.15, 'Ki': 0.0, 'Kd': 0.6}
-controller = Controller(gains)
-
-# is there a way to get the current error 
-control_action = controller.PD_control(error)
